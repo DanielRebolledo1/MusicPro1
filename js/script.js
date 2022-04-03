20b73d3c394dd253dd08f2a846c0020486368d28
Luciano Uribe <luc.uribe@duocuc.cl>
2022-04-03 18:32:27 -0400

    Cambios en script.js y creación de sidebar en menú de inicio

--- js/script.js
+++ js/script.js
@@ -1,51 +1,322 @@
-//Validacion
+const body = document.body;
 
-const loginForm = document.getElementById('login-form')
-const loginEmail = document.getElementById('login-email')
-const loginPassword = document.getElementById('login-pw')
+if (body.classList.contains('home')) {
+    console.log('home')
 
-const registerForm = document.getElementById('register-form')
-const registerName = document.getElementById('register-name')
-const registerLastName = document.getElementById('register-lastname')
-const registerEmail = document.getElementById('register-email')
-const registerPassword = document.getElementById('register-pw')
+    const subsForm = document.getElementById('subs-form')
+    const subsEmail = document.getElementById('subs-email')
 
-const forgotPasswordForm = document.getElementById('forgot-pw-form')
-const forgotPasswordEmail = document.getElementById('forgot-pw-email')
+    const categoryShowIcon1 = document.getElementById('show-icon-1');
+    const categoryShowIcon2 = document.getElementById('show-icon-2');
+    const categoryShowIcon3 = document.getElementById('show-icon-3');
 
-loginForm.addEventListener('submit', (e) => {
-    if(!checkLoginInputsOnSubmit()){
-        e.preventDefault();
-    }
-});
+    subsForm.addEventListener('submit', (e) => {
+        if(!checkSubsInputsOnSubmit(subsEmail)){
+            e.preventDefault();
+        }
+    });
 
-forgotPasswordForm.addEventListener('submit', (e) => {
-    if(!checkForgotPasswordInputsOnSubmit()){
-        e.preventDefault();
-    }
-});
+    subsEmail.addEventListener('focusout', () => {
+        checkEmailOnFocusOut(subsEmail);
+    });
 
-loginEmail.addEventListener('focusout', () => {
-    checkEmailOnFocusOut(loginEmail);
-});
+    subsEmail.addEventListener('input', () => {
+        resetInput(subsEmail);
+    });
 
-loginEmail.addEventListener('input', () => {
-    resetInput(loginEmail);
-});
+    //Carousels configuration (Swiper)
+    const swiper1 = new Swiper('.swiper1', {
+        // Optional parameters
+        direction: 'horizontal',
+        rewind: true,
+        speed: 1000,
 
-loginPassword.addEventListener('input', () => {
-    resetInput(loginPassword);
-});
+        // Autoplay
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
 
-forgotPasswordEmail.addEventListener('focusout', () => {
-    checkEmailOnFocusOut(forgotPasswordEmail);
-});
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination1',
+            clickable: true,
+        },
 
-forgotPasswordEmail.addEventListener('input', () => {
-    resetInput(forgotPasswordEmail);
-});
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+    const swiper2 = new Swiper('.swiper2', {
+        grabCursor: true,
+        clickable: false,
+        slidesPerView: 1,
+        speed: 500,
+        autoHeight: true,
+        // Optional parameters
+        direction: 'horizontal',
 
-function checkLoginInputsOnSubmit() {
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true,
+        },
+
+        breakpoints: {
+            // when window width is >= 1400px
+            1400: {
+                slidesPerView: 6,
+                slidesPerGroup: 6,
+            },
+            1200: {
+                slidesPerView: 5,
+                slidesPerGroup: 5,
+            },
+            900: {
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+            },
+            768: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+            },
+            620: {
+                slidesPerView: 2.25,
+                slidesPerGroup: 2,
+            },
+            560: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+            },
+            500: {
+                slidesPerView: 1.75,
+                slidesPerGroup: 1,
+            },
+            430: {
+                slidesPerView: 1.5,
+                slidesPerGroup: 1,
+            },
+            360: {
+                slidesPerView: 1.25,
+                slidesPerGroup: 1,
+            },
+        }
+    });
+    const swiper3 = new Swiper('.swiper3', {
+        grabCursor: true,
+        clickable: false,
+        slidesPerView: 1,
+        speed: 500,
+        autoHeight: true,
+        // Optional parameters
+        direction: 'horizontal',
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true,
+        },
+
+        breakpoints: {
+            // when window width is >= 1400px
+            1400: {
+                slidesPerView: 6,
+                slidesPerGroup: 6,
+            },
+            1200: {
+                slidesPerView: 5,
+                slidesPerGroup: 5,
+            },
+            900: {
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+            },
+            768: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+            },
+            620: {
+                slidesPerView: 2.25,
+                slidesPerGroup: 2,
+            },
+            560: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+            },
+            500: {
+                slidesPerView: 1.75,
+                slidesPerGroup: 1,
+            },
+            430: {
+                slidesPerView: 1.5,
+                slidesPerGroup: 1,
+            },
+            360: {
+                slidesPerView: 1.25,
+                slidesPerGroup: 1,
+            },
+        }
+    });
+
+    //JQuery functions
+    $(document).on('click', '#sidebar-open-btn', function() {
+
+        $('#sidebar-container').show();
+        $('#sidebar').show("slide", 300);
+        $('body').css('overflow', 'hidden');
+        $('#sidebar').css('overflow', 'auto');
+    });
+
+    $(document).on('click', '#sidebar-close-btn', function() {
+        $('#sidebar-container').hide("fade", 300);
+        $('#sidebar').hide("slide", 300);
+        $('body').css('overflow', 'auto');
+
+        if (categoryShowIcon1.classList.contains('hide')){
+            toggleCategoryShow($('#category-1-sub'), categoryShowIcon1);
+        }
+
+        if (categoryShowIcon2.classList.contains('hide')){
+            toggleCategoryShow($('#category-2-sub'), categoryShowIcon2);
+        }
+
+        if (categoryShowIcon3.classList.contains('hide')){
+            toggleCategoryShow($('#category-3-sub'), categoryShowIcon3);
+        }
+    });
+
+    $(document).on('click', '#sidebar-bg', function() {
+        $('#sidebar-close-btn').click();
+    });
+
+    $(document).on('click', '#category-1-btn', function() {
+        toggleCategoryShow($('#category-1-sub'), categoryShowIcon1);
+    });
+
+    $(document).on('click', '#category-2-btn', function() {
+        toggleCategoryShow($('#category-2-sub'), categoryShowIcon2);
+    });
+
+    $(document).on('click', '#category-3-btn', function() {
+        toggleCategoryShow($('#category-3-sub'), categoryShowIcon3);
+    });
+}
+
+if (body.classList.contains('login-register')){
+    console.log('login/register')
+
+    const loginForm = document.getElementById('login-form')
+    const loginEmail = document.getElementById('login-email')
+    const loginPassword = document.getElementById('login-pw')
+    const registerForm = document.getElementById('register-form')
+    const registerName = document.getElementById('register-name')
+    const registerLastName = document.getElementById('register-lastname')
+    const registerEmail = document.getElementById('register-email')
+    const registerPassword = document.getElementById('register-pw')
+    const forgotPasswordForm = document.getElementById('forgot-pw-form')
+    const forgotPasswordEmail = document.getElementById('forgot-pw-email')
+    const loginPasswordToggleBtn = document.getElementById('login-sh-btn')
+    const registerPasswordToggleBtn = document.getElementById('register-sh-btn')
+
+    loginForm.addEventListener('submit', (e) => {
+        if(!checkLoginInputsOnSubmit(loginEmail, loginPassword)){
+            e.preventDefault();
+        }
+    });
+
+    forgotPasswordForm.addEventListener('submit', (e) => {
+        if(!checkForgotPasswordInputsOnSubmit(forgotPasswordEmail)){
+            e.preventDefault();
+        }
+    });
+
+    loginEmail.addEventListener('focusout', () => {
+        checkEmailOnFocusOut(loginEmail);
+    });
+
+    loginEmail.addEventListener('input', () => {
+        resetInput(loginEmail);
+    });
+
+    loginPassword.addEventListener('input', () => {
+        resetInput(loginPassword);
+    });
+
+    forgotPasswordEmail.addEventListener('focusout', () => {
+        checkEmailOnFocusOut(forgotPasswordEmail);
+    });
+
+    forgotPasswordEmail.addEventListener('input', () => {
+        resetInput(forgotPasswordEmail);
+    });
+
+    registerForm.addEventListener('submit', (e) => {
+        if(!checkRegisterInputsOnSubmit(registerEmail, registerPassword, registerName, registerLastName)){
+            e.preventDefault();
+        }
+    });
+
+    registerEmail.addEventListener('focusout', () => {
+        checkEmailOnFocusOut(registerEmail);
+    });
+
+    registerEmail.addEventListener('input', () => {
+        resetInput(registerEmail);
+    });
+
+    registerName.addEventListener('input', () => {
+        resetInput(registerName);
+    });
+
+    registerLastName.addEventListener('input', () => {
+        resetInput(registerLastName);
+    });
+
+    registerPassword.addEventListener('input', () => {
+        resetInput(registerPassword);
+    });
+
+    loginPasswordToggleBtn.addEventListener('click', () => {
+        togglePasswordView(loginPassword, loginPasswordToggleBtn);
+    });
+
+    registerPasswordToggleBtn.addEventListener('click', () => {
+        togglePasswordView(registerPassword, registerPasswordToggleBtn);
+    });
+
+    //JQuery functions
+    jQuery(document).on('click', '#forgot-pw-btn', function() {
+        jQuery('#login-register-tabs, #forgot-pw-tab').toggle();
+        resetInput(loginEmail);
+        resetInput(loginPassword);
+    });
+
+    jQuery(document).on('click', '#forgot-pw-back-btn', function() {
+        jQuery('#login-register-tabs, #forgot-pw-tab').toggle();
+        resetInput(forgotPasswordEmail);
+    });
+
+    jQuery(document).on('click', '#register-shortcut-btn', function() {
+        const registerTab = document.getElementById('pills-register-tab');
+        registerTab.click();
+    });
+}
+
+//Validaciones de formularios
+
+function checkLoginInputsOnSubmit(loginEmail, loginPassword) {
     //get values from the inputs
     const emailValue = loginEmail.value.trim();
     const passwordValue = loginPassword.value.trim();
@@ -69,7 +340,7 @@ function checkLoginInputsOnSubmit() {
     return checkSuccess(loginEmail) && checkSuccess(loginPassword);
 }
 
-function checkForgotPasswordInputsOnSubmit() {
+function checkForgotPasswordInputsOnSubmit(forgotPasswordEmail) {
     //get values from the inputs
     const emailValue = forgotPasswordEmail.value.trim();
 
@@ -86,13 +357,66 @@ function checkForgotPasswordInputsOnSubmit() {
     return checkSuccess(forgotPasswordEmail);
 }
 
+function checkRegisterInputsOnSubmit(registerEmail, registerPassword, registerName, registerLastName) {
+    //get values from the inputs
+    const emailValue = registerEmail.value.trim();
+    const passwordValue = registerPassword.value.trim();
+    const nameValue = registerName.value.trim();
+    const lastNameValue = registerLastName.value.trim();
+
+    if(emailValue === '') {
+        //add error class
+        setErrorFor(registerEmail, 'Por favor ingrese un email');
+    } else if(!isEmail(emailValue)) {
+        setErrorFor(registerEmail, 'Por favor ingrese un email válido');
+    } else {
+        //add success class
+        setSuccessFor(registerEmail);
+    }
+
+    if(passwordValue === '') {
+        setErrorFor(registerPassword, 'Por favor ingrese una contraseña');
+    } else {
+        setSuccessFor(registerPassword);
+    }
+
+    if(nameValue === '') {
+        setErrorFor(registerName, 'Por favor ingrese su nombre');
+    } else {
+        setSuccessFor(registerName);
+    }
+
+    if(lastNameValue === '') {
+        setErrorFor(registerLastName, 'Por favor ingrese su apellido');
+    } else {
+        setSuccessFor(registerLastName);
+    }
+
+    return checkSuccess(registerEmail) && checkSuccess(registerPassword) && checkSuccess(registerName) && checkSuccess(registerLastName) ;
+}
+
+function checkSubsInputsOnSubmit(subsEmail) {
+    //get values from the inputs
+    const emailValue = subsEmail.value.trim();
+
+    if(emailValue === '') {
+        //add error class
+        setErrorFor(subsEmail, 'Por favor ingrese un email');
+    } else if(!isEmail(emailValue)) {
+        setErrorFor(subsEmail, 'Por favor ingrese un email válido');
+    } else {
+        //add success class
+        setSuccessFor(subsEmail);
+    }
+
+    return checkSuccess(subsEmail);
+}
+
 function checkEmailOnFocusOut(email) {
     const emailValue = email.value.trim();
 
     if(!isEmail(emailValue) && emailValue!=='') {
         setErrorFor(email, 'Por favor ingrese un email válido');
-    } else {
-        resetInput(email);
     }
 }
 
@@ -135,17 +459,6 @@ function checkSuccess(input){
 
 //Ver contrasena/ocultar
 
-const loginPasswordToggleBtn = document.getElementById('login-sh-btn')
-const registerPasswordToggleBtn = document.getElementById('register-sh-btn')
-
-loginPasswordToggleBtn.addEventListener('click', () => {
-    togglePasswordView(loginPassword, loginPasswordToggleBtn);
-});
-
-registerPasswordToggleBtn.addEventListener('click', () => {
-    togglePasswordView(registerPassword, registerPasswordToggleBtn);
-});
-
 function togglePasswordView(password, button){
     if (password.type === 'password'){
         password.type = 'text';
@@ -156,122 +469,16 @@ function togglePasswordView(password, button){
     }
 }
 
-//Registrarse
+//Ver subcategorias/ocultar
 
-registerForm.addEventListener('submit', (e) => {
-    if(!checkRegisterInputsOnSubmit()){
-        e.preventDefault();
-    }
-});
+function toggleCategoryShow(category, icon){
+    category.slideToggle(300);
 
-registerEmail.addEventListener('focusout', () => {
-    checkEmailOnFocusOut(registerEmail);
-});
-
-registerEmail.addEventListener('input', () => {
-    resetInput(registerEmail);
-});
-
-registerName.addEventListener('input', () => {
-    resetInput(registerName);
-});
-
-registerLastName.addEventListener('input', () => {
-    resetInput(registerLastName);
-});
-
-registerPassword.addEventListener('input', () => {
-    resetInput(registerPassword);
-});
-
-function checkRegisterInputsOnSubmit() {
-    //get values from the inputs
-    const emailValue = registerEmail.value.trim();
-    const passwordValue = registerPassword.value.trim();
-    const nameValue = registerName.value.trim();
-    const lastNameValue = registerLastName.value.trim();
-
-    if(emailValue === '') {
-        //add error class
-        setErrorFor(registerEmail, 'Por favor ingrese un email');
-    } else if(!isEmail(emailValue)) {
-            setErrorFor(registerEmail, 'Por favor ingrese un email válido');
+    if (icon.classList.contains('hide')){
+        icon.classList.remove('hide');
+        icon.classList.add('show');
     } else {
-        //add success class
-        setSuccessFor(registerEmail);
+        icon.classList.remove('show');
+        icon.classList.add('hide');
     }
-
-    if(passwordValue === '') {
-        setErrorFor(registerPassword, 'Por favor ingrese una contraseña');
-    } else {
-        setSuccessFor(registerPassword);
-    }
-
-    if(nameValue === '') {
-        setErrorFor(registerName, 'Por favor ingrese su nombre');
-    } else {
-        setSuccessFor(registerName);
-    }
-
-    if(lastNameValue === '') {
-        setErrorFor(registerLastName, 'Por favor ingrese su apellido');
-    } else {
-        setSuccessFor(registerLastName);
-    }
-
-    return checkSuccess(registerEmail) && checkSuccess(registerPassword) && checkSuccess(registerName) && checkSuccess(registerLastName) ;
 }
-
-//Funciones JQuery
-
-jQuery(document).on('click', '#forgot-pw-btn', function() {
-    jQuery('#login-register-tabs, #forgot-pw-tab').toggle();
-    resetInput(loginEmail);
-    resetInput(loginPassword);
-});
-
-jQuery(document).on('click', '#forgot-pw-back-btn', function() {
-    jQuery('#login-register-tabs, #forgot-pw-tab').toggle();
-    resetInput(forgotPasswordEmail);
-});
-
-jQuery(document).on('click', '#register-shortcut-btn', function() {
-    const registerTab = document.getElementById('pills-register-tab');
-    registerTab.click();
-});
-
-/*
-const subsForm = document.getElementById('subs-form')
-const subsEmail = document.getElementById('subs-email')
-
-subsForm.addEventListener('submit', (e) => {
-    if(!checkSubsInputsOnSubmit()){
-        e.preventDefault();
-    }
-});
-
-subsEmail.addEventListener('focusout', () => {
-    checkEmailOnFocusOut(subsEmail);
-});
-
-subsEmail.addEventListener('input', () => {
-    resetInput(subsEmail);
-});
-
-function checkSubsInputsOnSubmit() {
-    //get values from the inputs
-    const emailValue = subsEmail.value.trim();
-
-    if(emailValue === '') {
-        //add error class
-        setErrorFor(subsEmail, 'Por favor ingrese un email');
-    } else if(!isEmail(emailValue)) {
-        setErrorFor(subsEmail, 'Por favor ingrese un email válido');
-    } else {
-        //add success class
-        setSuccessFor(subsEmail);
-    }
-
-    return checkSuccess(subsEmail);
-}
-*/
