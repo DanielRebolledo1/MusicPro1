801f071e34c7fc7da3ee37ba07912d115a2002ff
Luciano Uribe <luc.uribe@duocuc.cl>
2022-06-12 20:20:01 -0400

    Mejoras en API Rest

--- zuzuPlayDjango/rest_categoriaPromo/models.py
+++ zuzuPlayDjango/rest_categoriaPromo/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Categoria_Promocional(models.Model):
     id = models.AutoField(primary_key=True, verbose_name='Id')
     nombre = models.CharField(max_length=50, verbose_name='Nombre')
+    imagen = models.ImageField(upload_to='rest_categoriaPromo/media', verbose_name='Imagen')
 
     def __str__(self):
         return self.nombre
--- zuzuPlayDjango/rest_categoriaPromo/views.py
+++ zuzuPlayDjango/rest_categoriaPromo/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from .models import Categoria_Promocional
 from .serializers import CategoriaPromoSerializer
+
 
 # Create your views here.
 @csrf_exempt
@@ -16,10 +16,32 @@ def categorias_promo(request):
         serializer = CategoriaPromoSerializer(categorias, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
-        dataParse = JSONParser().parse(request)
-        serializer = CategoriaPromoSerializer(data=dataParse)
+        serializer = CategoriaPromoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
+def modificar_categorias_promo(request, id):
+    try:
+        categoria = Categoria_Promocional.objects.get(id=id)
+    except Categoria_Promocional.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CategoriaPromoSerializer(categoria)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CategoriaPromoSerializer(categoria, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        categoria.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
