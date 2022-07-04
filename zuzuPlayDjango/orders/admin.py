736c9ffd632250062e085014453b6403f1a66b8a
Luciano Uribe <luc.uribe@duocuc.cl>
2022-07-04 00:41:32 -0400

    Nuevos modelos en admin

--- zuzuPlayDjango/orders/admin.py
+++ zuzuPlayDjango/orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Orden, Descuento, Cupon, Region, Comuna, Direccion, Despacho, Pago
+
 
 # Register your models here.
+admin.site.register(Orden)
+admin.site.register(Descuento)
+admin.site.register(Cupon)
+admin.site.register(Region)
+admin.site.register(Comuna)
+admin.site.register(Direccion)
+admin.site.register(Despacho)
+admin.site.register(Pago)
